6da9c80bce6c201c20db9fdb47db2a9ac41e7ce0
ElenaHammer <92867630+ElenaHammer@users.noreply.github.com>
2023-01-14 15:50:39 +0100

    Update homepage.js

--- frontend/src/components/header/homepage.js
+++ frontend/src/components/header/homepage.js
@@ -1,3 +1,14 @@
 /* Class-based Components can't use React Hooks */
+/* Die Homepage Seite ist die Hauptseite dieser Applikation */
+
+import * as React from 'react';
 
 
+class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+        this. state = {};
+    }
+
+
+}
