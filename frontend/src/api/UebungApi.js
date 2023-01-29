86f55f8cc369a0594c31ce0a80669ddde166b743
ElenaHammer <92867630+ElenaHammer@users.noreply.github.com>
2023-01-29 09:26:51 +0100

    Update UebungApi.js
    Kommentare mit Erklärungen

--- frontend/src/api/UebungApi.js
+++ frontend/src/api/UebungApi.js
@@ -1,14 +1,19 @@
+// Routen verweisen auf das Backend (main.py - Service Layer)
 import PersonBO from "./PersonBO";
 
 export default class UebungApi {
 
     // Singelton
+    // Die Klasse verwendet das Singleton-Muster, um sicherzustellen, dass es immer nur eine Instanz von ihr gibt,
+    // die wiederverwendet werden kann.
     static #api = null;
 
     // Python-Backend
+    // Die Klasse enthält eine URL-Eigenschaft "uebungServerURL", die auf den Server für die Uebung-Anwendung zeigt.
     #uebungServerURL = '/uebung';
 
     // Person
+    // Eine weitere Eigenschaft "getPersonenURL" erzeugt eine vollständige URL für den Zugriff auf die Person-Ressource.
     #getPersonenURL = () => `${this.#uebungServerURL}/person`;
 
     static getAPI() {
@@ -18,6 +23,8 @@ export default class UebungApi {
         return this.#api;
     }
 
+    // Die Methode "fetchAdvanced" ist eine generische Methode, die auf eine URL (url) zugreift und
+    // ein Promise zurückgibt, das bei erfolgreicher Abfrage den Inhalt der Antwort im JSON-Format enthält.
     #fetchAdvanced = (url, init) => fetch(url, init)
     .then(res => {
         if (!res.ok) {
@@ -28,6 +35,9 @@ export default class UebungApi {
     )
 
     // Person
+    // Die Methode "getPersonen" ruft die Personen-Ressource über die URL von "getPersonenURL" auf und verarbeitet
+    // die Antwort mit "fetchAdvanced". Die Antwort wird in ein JSON-Format umgewandelt und an die Klasse "PersonBO"
+    // übergeben, um ein Business Object (BO) zu erzeugen. Am Ende wird das erzeugte BO als Promise zurückgegeben.
     getPersonen() {
         return this.#fetchAdvanced(this.#getPersonenURL()).then((responseJSON) => {
             let personenBO = PersonBO.fromJSON(responseJSON);
